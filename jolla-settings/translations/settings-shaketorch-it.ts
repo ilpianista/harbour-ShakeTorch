b985c9aa3f38a9a9113e92040c55027d55c4fe98
Andrea Scarpino <andrea@scarpino.dev>
2026-04-04 09:21:14 +0200

    Translated using Weblate (Italian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Harbour ShakeTorch/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-shaketorch/translations/it/

--- jolla-settings/translations/settings-shaketorch-it.ts
+++ jolla-settings/translations/settings-shaketorch-it.ts
@@ -6,23 +6,23 @@
     <message id="settings_shaketorch_entryname">
         <source>ShakeTorch</source>
         <extracomment>name in the Settings Page</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ShakeTorch</translation>
     </message>
     <message id="settings_shaketorch_startstop_desc">
         <source>Turn on the flashlight when the device is shaken.</source>
         <extracomment>description of the start/stop switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Accendi la torcia scuotendo il telefono.</translation>
     </message>
     <message id="settings_shaketorch_enable_desc">
         <source>Enable and start at Boot</source>
         <oldsource>Enable at Boot</oldsource>
         <extracomment>description of the enable switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Attiva automaticamente all&apos;avvio</translation>
     </message>
     <message id="settings_shaketorch_eventname">
         <source>ShakeTorch</source>
         <extracomment>button name in the top menu</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>ShakeTorch</translation>
     </message>
 </context>
 </TS>
