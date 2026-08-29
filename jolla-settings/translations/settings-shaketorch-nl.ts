996b22b5080ae64cf596a1ccc96a87d313ec5e91
Nathan <nthn@unseen.is>
2026-08-29 16:43:09 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Harbour ShakeTorch/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-shaketorch/translations/nl/

--- jolla-settings/translations/settings-shaketorch-nl.ts
+++ jolla-settings/translations/settings-shaketorch-nl.ts
@@ -6,23 +6,23 @@
     <message id="settings_shaketorch_entryname">
         <source>ShakeTorch</source>
         <extracomment>name in the Settings Page</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Schudlamp</translation>
     </message>
     <message id="settings_shaketorch_startstop_desc">
         <source>Turn on the flashlight when the device is shaken.</source>
         <extracomment>description of the start/stop switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zet de zaklamp aan wanneer je het apparaat schudt.</translation>
     </message>
     <message id="settings_shaketorch_enable_desc">
         <source>Enable and start at Boot</source>
         <oldsource>Enable at Boot</oldsource>
         <extracomment>description of the enable switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Inschakelen en starten bij opstarten</translation>
     </message>
     <message id="settings_shaketorch_eventname">
         <source>ShakeTorch</source>
         <extracomment>button name in the top menu</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Schudlamp</translation>
     </message>
 </context>
 </TS>
